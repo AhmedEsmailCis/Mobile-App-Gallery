1b0a652fec95cacbac3d3b06fd4224ec5a453058
Ahmed Esmail Abdullah <TheGolden-Eagle@users.noreply.github.com>
2021-03-21 15:41:04 +0200

    rearrang the functions that connect with APIs to minimiz connection with it

--- app/screens/AlbumsScreen.js
+++ app/screens/AlbumsScreen.js
@@ -17,77 +17,57 @@ export default class AlbumsScreen extends Component {
       pickerValue: -1,
       generationArray: [],
       loader: true,
+      dataOfUsers: [],
     };
   }
-  componentDidMount() {
+  componentDidMount = async () => {
+    this.loadUserData();
     this.loadData(this.state.pickerValue);
-  }
+  };
   loadData = async (pickerValue) => {
     this.setState({loader: true});
+    const dataOfAlbums = await this.loadAlbumsData(pickerValue);
+    let {generationArray, dataOfUsers} = this.state;
+    generationArray = [];
+    for (let i = 0; i < dataOfAlbums.length; i++) {
+      const photoUrl =
+        'https://jsonplaceholder.typicode.com/photos?albumId=' +
+        dataOfAlbums[i].id.toString();
+      const requestPhotoData = await Axios.get(photoUrl);
+      for (let j = 0; j < dataOfUsers.length; j++) {
+        if (dataOfUsers[j].id === dataOfAlbums[i].userId) {
+          const dataObj = {
+            userObj: dataOfUsers[j],
+            albumData: dataOfAlbums[i],
+            photosOfAlbum: requestPhotoData.data,
+          };
+          generationArray.push(dataObj);
+          break;
+        }
+      }
+    }
+    this.setState({loader: false, generationArray});
+  };
+  loadAlbumsData = async (pickerValue) => {
     const albumUrl =
       pickerValue === -1
         ? 'https://jsonplaceholder.typicode.com/albums'
         : 'https://jsonplaceholder.typicode.com/albums?userId=' +
           pickerValue.toString();
     const requestAlbumsData = await Axios.get(albumUrl);
-    let dataOfAlbums = requestAlbumsData.data;
-    //---------------------------------------------------------------------
-    const userUrl =
-      pickerValue === -1
-        ? 'https://jsonplaceholder.typicode.com/users'
-        : 'https://jsonplaceholder.typicode.com/users?id=' +
-          pickerValue.toString();
-    const requestUserData = await Axios.get(userUrl);
-    let dataOfUsers = requestUserData.data;
-    //----------------------------------------------------------------------
-    let dataOfPhotos = [];
-    for (let index = 0; index < dataOfAlbums.length; index++) {
-      const photoUrl =
-        'https://jsonplaceholder.typicode.com/photos?albumId=' +
-        dataOfAlbums[index].id.toString();
-      const requestPhotoData = await Axios.get(photoUrl);
-      dataOfPhotos = dataOfPhotos.concat(requestPhotoData.data);
-    }
-    //----------------------------------------------------------------------
-    //Generation the Array of Albums Section
-    let gArray = [];
-    for (let i = 0; i < dataOfAlbums.length; i++) {
-      let photoData = [];
-      for (let j = 0; j < dataOfPhotos.length; j++) {
-        if (dataOfAlbums[i].id === dataOfPhotos[j].albumId) {
-          const photoObj = {
-            photoId: dataOfPhotos[j].id,
-            photoTitle: dataOfPhotos[j].title,
-            url: dataOfPhotos[j].url,
-            thumbnailUrl: dataOfPhotos[j].thumbnailUrl,
-          };
-          photoData.push(photoObj);
-        }
-      }
-      for (let j = 0; j < dataOfUsers.length; j++) {
-        if (dataOfUsers[j].id === dataOfAlbums[i].userId) {
-          const dataObj = {
-            userId: dataOfUsers[j].id,
-            albumId: dataOfAlbums[i].id,
-            name: dataOfUsers[j].name,
-            website: dataOfUsers[j].website,
-            email: dataOfUsers[j].email,
-            albumTitle: dataOfAlbums[i].title,
-            photosOfAlbum: photoData,
-          };
-          gArray.push(dataObj);
-          break;
-        }
-      }
-    }
-    //----------------------------------------------------------------------
-    this.setState({loader: false, generationArray: gArray});
+    return requestAlbumsData.data;
   };
-  onValueChangeForPicker = (itemValue, itemIndex) => {
+  loadUserData = async () => {
+    const request = await Axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    this.setState({dataOfUsers: request.data});
+  };
+  onValueChangeForPicker = (itemValue, _) => {
     this.setState({pickerValue: itemValue});
     this.loadData(itemValue);
   };
-  _keyExtractor = (item, _) => item.albumId.toString();
+  _keyExtractor = (item, _) => item.albumData.id.toString();
   render() {
     return (
       <View style={styles.albumsScreen}>
@@ -95,10 +75,11 @@ export default class AlbumsScreen extends Component {
         <Header
           onValueChange={this.onValueChangeForPicker}
           selectedValue={this.state.pickerValue}
+          dataOfUsers={this.state.dataOfUsers}
         />
         {this.state.loader ? (
           <View style={styles.loaderStyle}>
-            <ActivityIndicator size="large" color="green" />
+            <ActivityIndicator testID="loader" size="large" color="green" />
           </View>
         ) : (
           <FlatList
@@ -106,12 +87,8 @@ export default class AlbumsScreen extends Component {
             data={this.state.generationArray}
             renderItem={({item}) => (
               <AlbumSection
-                userId={item.userId}
-                albumId={item.albumId}
-                name={item.name}
-                website={item.website}
-                email={item.email}
-                albumTitle={item.albumTitle}
+                userObj={item.userObj}
+                albumData={item.albumData}
                 photosOfAlbum={item.photosOfAlbum}
                 navigation={this.props.navigation}
               />
